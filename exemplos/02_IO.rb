de2f9fe712d3a8e245d4912af31851ae7fa16c96
Monica Craveiro <craveiromonica@gmail.com>
2024-09-22 23:34:53 -0300

    IRB, Pry, Variáveis, STDIN, STDOUT

---
+++ exemplos/02_IO.rb
@@ -0,0 +1,8 @@
+puts "Digite seu nome: "
+nome = gets
+puts "Seu nome é: " + nome
+puts nome.inspect
+puts "================================================"
+puts "Digite sua idade: "
+idade = gets
+puts "Seu nome é #{nome} e sua idade é #{idade}"
